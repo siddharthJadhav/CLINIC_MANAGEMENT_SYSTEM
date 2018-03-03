e24767887f777eeb05400dbeac580a927d2ac165
Siddharth Jadhav <siddharth.jadhav2093@gmail.com>
2018-03-03 22:52:56 +0530

    resize of card page

--- src/clinic/card.java
+++ src/clinic/card.java
@@ -117,6 +117,7 @@ public class card extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Card");
+        setPreferredSize(new java.awt.Dimension(780, 739));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
